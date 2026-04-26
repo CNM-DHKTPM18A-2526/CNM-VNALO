7af9309f7940328987d36a1e00069546a62547dc
iamnguyenvu <iamnguyenvu.gm@gmail.com>
2026-04-26 10:44:59 +0700

    fix: ensure proper HTTP security configuration by returning the built HttpSecurity object

--- backend/java-services/services/analytics-service/src/main/java/iuh/cnm/vnalo/analytics_service/config/SecurityConfig.java
+++ backend/java-services/services/analytics-service/src/main/java/iuh/cnm/vnalo/analytics_service/config/SecurityConfig.java
@@ -60,6 +60,7 @@ public class SecurityConfig {
                 .addFilterBefore(internalServiceAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
+        return http.build();
     }
 
     @Bean
